6eb1ce6fd9258f523d901807664526cf230f2d0a
LASSE <lassehovden@gmail.com>
2019-10-16 20:28:16 +0200

    added new class Projectmemberdao

--- src/main/java/no/kristiania/ProjectMemberDao.java
+++ src/main/java/no/kristiania/ProjectMemberDao.java
@@ -1,4 +1,12 @@
 package no.kristiania;
 
+import java.util.List;
+
 public class ProjectMemberDao {
+    public void insertMember(String johannes) {
+    }
+
+    public List<String> listall() {
+        return null;
+    }
 }
--- src/test/java/no/kristiania/ProjectmemberTest.java
+++ src/test/java/no/kristiania/ProjectmemberTest.java
@@ -4,11 +4,13 @@ package java.no.kristiania;
 import no.kristiania.ProjectMemberDao;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 public class ProjectmemberTest {
 
     @Test
      void shouldRetriveProjectMember(){
-        ProjectMemberDao dao= new ProjectMemberDao;
+        ProjectMemberDao dao= new ProjectMemberDao();
         dao.insertMember("Johannes");
         assertThat(dao.listall()).contains("Johannes");
 
