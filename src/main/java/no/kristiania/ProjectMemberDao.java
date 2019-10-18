44114086b77a804e164866960d080ed9d96f823a
LASSE <lassehovden@gmail.com>
2019-10-18 13:26:11 +0200

    refactoring methods in ProjectmemberDao

--- src/main/java/no/kristiania/ProjectMemberDao.java
+++ src/main/java/no/kristiania/ProjectMemberDao.java
@@ -3,13 +3,13 @@ package no.kristiania;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProjectMemberDao {
 
-    private List<String> projectMembers = new ArrayList<>();
     private DataSource dataSource;
 
     public ProjectMemberDao(DataSource dataSource) {
@@ -17,7 +17,6 @@ public class ProjectMemberDao {
     }
 
     public void insertMember(String memberName) {
-        projectMembers.add(memberName);
 
         try (Connection conn = dataSource.getConnection();) {
             PreparedStatement statement = conn.prepareStatement(
@@ -30,12 +29,22 @@ public class ProjectMemberDao {
 
     }
 
-    public List<String> listAll() {
+    public List<String> listAll() throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
-            connection.prepareStatement("select * from projectMembers");
-        } catch (SQLException e) {
-            e.printStackTrace();
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "select * from projectMembers"
+            )) {
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    List<String> result = new ArrayList<>();
+
+                    while (resultSet.next()) {
+                        result.add(resultSet.getString("name"));
+                    }
+                    return result;
+                }
+            }
         }
-        return projectMembers;
     }
 }
+
+
